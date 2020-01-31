f9cd8c6719e2f2eb6eea06105b4f415bdb2f18b2
Andrea(Shiny)Voscilla <andrea.voscilla@hotmail.it>
2020-02-01 00:21:37 +0100

    aiming system implemented

--- GGJ2020/Assets/Mooovment.cs
+++ GGJ2020/Assets/Mooovment.cs
@@ -5,17 +5,47 @@ using UnityEngine;
 public class Mooovment : MonoBehaviour
 {
     public float mooovementSpeed = 5.0f;
+    public GameObject CrosshairPivot;
+    public GameObject Crosshair;
+    bool resetCrosshairPosition = true;
 
-    
+
+
+
+    Vector3 cursorInitialPosition = new Vector3();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            if (resetCrosshairPosition)
+            {
+                resetCrosshairPosition = false;
+                Crosshair.transform.localPosition = new Vector3();
+                CrosshairPivot.transform.rotation = Quaternion.identity;
+                cursorInitialPosition = Input.mousePosition;
+
+            }
+
+            Crosshair.transform.position = (cursorInitialPosition - Input.mousePosition) * mooovementSpeed;
+
+
+        }
+
+        if (!Input.GetMouseButton(0))
+        {
+            //TODO: shoot behaviour
+           
+            resetCrosshairPosition = true;
+            Crosshair.transform.localPosition = new Vector3();
+            CrosshairPivot.transform.rotation = Quaternion.identity;     
+        }
     }
 }
