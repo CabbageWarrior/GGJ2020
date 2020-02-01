74f4ca7ecf4159148b4f55b153098f15d9316c0b
Verza-ROG <feelthecabbage>
2020-02-01 16:44:37 +0100

    Readded inputs for canvas.

---
+++ GGJ2020/Assets/0_ProjectData/Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void Play()
+    {
+        Debug.Log("Play");
+    }
+
+    public void Credits()
+    {
+        Debug.Log("Credits");
+    }
+
+    public void Info()
+    {
+        Debug.Log("Info");
+    }
+}
