97c385e6457a920b68f3c973b758c44b37363a65
Andrea(Shiny)Voscilla <andrea.voscilla@hotmail.it>
2020-02-01 14:21:33 +0100

    removed VaccaID(o)

--- GGJ2020/Assets/0_ProjectData/Scripts/CownonBallStatistics.cs
+++ GGJ2020/Assets/0_ProjectData/Scripts/CownonBallStatistics.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CownonBall", menuName = "ScriptableObjects/CownonBallStatistics", order = 1)]
 public class CownonBallStatistics : ScriptableObject
 {
-    public float shakeTime;
-    public float shakeDuration;
-    public float shakeIntensity;
+    [Space]
+    public Vector2 shakeTime;
+    public Vector2 shakeDuration;
+    public Vector2 shakeIntensity;
 
     [Space]
-    public Animator cownonballAnmator;
+    public Animator cownonballAnimator;
 }
