5ad641d474ab16e749d6b46253ded13c815cbe94
DaaMonkey <simonetomi@gmail.com>
2020-02-01 21:14:08 +0100

    Avventure incredibili con draghi spaziali

--- GGJ2020/Assets/0_ProjectData/Scripts/AudioManager.cs
+++ GGJ2020/Assets/0_ProjectData/Scripts/AudioManager.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    private static AudioManager instance;
+    private static AudioManager Instance { get { return instance; } }
+
     private AudioSource source;
 
     public AudioClip[] music;
     public AudioClip[] sfx;
     public AudioClip[] cowSound;
 
+    private void Awake()
+    {
+        instance = this;
+    }
+
     void Start()
     {
         source = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void PlayMusic(int audio)
     {
         source.PlayOneShot(music[audio]);
+    }
+
+    public void PlaySfx(int audio)
+    {
+        source.PlayOneShot(sfx[audio]);
+    }
+
+    public void PlayCowSound(int audio)
+    {
+        source.PlayOneShot(cowSound[audio]);
     }
 }
