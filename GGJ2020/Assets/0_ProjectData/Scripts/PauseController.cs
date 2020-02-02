2818adcf0a63dc456455d3316099a140258aee27
Verza-ROG <feelthecabbage>
2020-02-02 03:15:25 +0100

    Made pause menu.

---
+++ GGJ2020/Assets/0_ProjectData/Scripts/PauseController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    public FadePanelsController fadePanelsController = default;
+
+    public void Replay()
+    {
+        fadePanelsController.OnClose = () => {
+            SceneManager.LoadScene(1);
+        };
+        fadePanelsController.Close();
+    }
+
+    public void ReturnToMenu()
+    {
+        fadePanelsController.OnClose = () => {
+            SceneManager.LoadScene(0);
+        };
+        fadePanelsController.Close();
+    }
+}
